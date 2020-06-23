625ee40663a0f130b04d6f39bf0ea90231f14f75
nohtou <nohtou@student.42tokyo.jp>
2020-06-23 19:59:38 +0900

    Add memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,23 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nohtou <nohtou@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/23 01:30:38 by nohtou            #+#    #+#             */
-/*   Updated: 2020/06/23 19:49:58 by nohtou           ###   ########.fr       */
+/*   Created: 2020/06/23 18:01:41 by nohtou            #+#    #+#             */
+/*   Updated: 2020/06/23 19:57:59 by nohtou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
 #include <stddef.h>
 
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+	int					re;
 
-#endif
+	if (!dst && !src && len)
+		return (dst);
+	re = dst > src;
+	d = re ? dst + len - 1 : dst;
+	s = re ? src + len - 1 : src;
+	while (len--) {
+		if (re)
+			*d-- = *s--;
+		else
+			*d++ = *s++;
+	}
+	return (dst);
+}
