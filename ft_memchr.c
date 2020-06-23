e1347d8fb1655ccb32eeb60a6e935193cc0a32e5
nohtou <nohtou@student.42tokyo.jp>
2020-06-24 00:54:07 +0900

    Add memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nohtou <nohtou@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/23 01:30:38 by nohtou            #+#    #+#             */
-/*   Updated: 2020/06/23 21:38:55 by nohtou           ###   ########.fr       */
+/*   Created: 2020/06/24 00:40:03 by nohtou            #+#    #+#             */
+/*   Updated: 2020/06/24 00:53:09 by nohtou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <stddef.h>
 
-# include <stddef.h>
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char *p;
 
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-
-#endif
+	p = (unsigned char *)s;
+	while (n--)
+		if (*p++ == (unsigned char)c)
+			return (--p);
+	return (NULL);
+}
