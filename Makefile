95025cda56c3b9a8dfc19eb3ed2d6736dd6e357a
nohtou <nohtou@student.42tokyo.jp>
2020-07-01 23:55:36 +0900

    Add strmapi

--- Makefile
+++ Makefile
@@ -27,7 +27,8 @@ ft_strdup.c \
 ft_substr.c \
 ft_strjoin.c \
 ft_strtrim.c \
-ft_split.c
+ft_split.c \
+ft_strmapi.c
 OBJS=$(SRCS:.c=.o)
 
 .PHONY: all clean fclean re
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nohtou <nohtou@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/23 01:30:38 by nohtou            #+#    #+#             */
-/*   Updated: 2020/06/30 01:09:51 by nohtou           ###   ########.fr       */
+/*   Updated: 2020/07/01 23:54:35 by nohtou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,6 @@ char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 #endif
