c0d5dbc90819328957484cdd7faf34a8b3493d0a
nohtou <nohtou@student.42tokyo.jp>
2020-07-05 14:53:00 +0900

    Mod bzero use memset

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,17 +6,13 @@
 /*   By: nohtou <nohtou@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/23 14:30:26 by nohtou            #+#    #+#             */
-/*   Updated: 2020/06/23 21:06:22 by nohtou           ###   ########.fr       */
+/*   Updated: 2020/07/05 14:51:36 by nohtou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
-	unsigned char *p;
-
-	p = s;
-	while (n--)
-		*p++ = 0;
+	ft_memset(s, 0, n);
 }
